28e25112e3a74e5c0536d16c0736610a62b80101
Jonathan White <jawh@umich.edu>
2024-10-26 16:20:19 -0600

    Replaced atomic_bool stop flag with std::stop_source member.

--- src/ThreadPool.hpp
+++ src/ThreadPool.hpp
@@ -22,48 +22,64 @@
 class ThreadPool {
  public:
   explicit ThreadPool(int n_threads) : m_tasks_ready(n_threads) {
+    auto stop_tkn = std::stop_token{m_stop_source.get_token()};
     for (auto thread_id = 0; thread_id < n_threads; ++thread_id) {
-      m_threads.emplace_back([this, thread_id, n_threads] {
-        auto old_n_items = 0;
-        auto thread_begin = 0;
-        auto thread_end = 0;
-        while (true) {
-          for (auto trial = 0; !m_stop and not(m_tasks_ready[thread_id]);
-               ++trial) {
-            if (trial == 8) {
-              trial = 0;
-              std::this_thread::yield();
+      m_threads.emplace_back(
+          [this, thread_id, n_threads](std::stop_token st) {
+            auto old_n_items = 0;
+            auto thread_begin = 0;
+            auto thread_end = 0;
+            while (true) {
+              for (auto trial = 0;
+                   not st.stop_requested() and not(m_tasks_ready[thread_id]);
+                   ++trial) {
+                DEBUG_LOG(std::this_thread::get_id()
+                          << ": in spinlock; stop requested: "
+                          << st.stop_requested()
+                          << "; m_tasks_ready[thread_id]: "
+                          << m_tasks_ready[thread_id])
+                if (trial == 8) {
+                  trial = 0;
+                  std::this_thread::yield();
+                }
+              }
+              if (st.stop_requested()) {
+                DEBUG_LOG(std::this_thread::get_id()
+                          << ": stop requested, terminating loop")
+                break;
+              }
+              m_tasks_ready[thread_id] = false;
+
+              if (auto current_n_items = m_current_n_items.load();
+                  current_n_items != old_n_items) {
+                old_n_items = current_n_items;
+                auto items_per_thread =
+                    (current_n_items + n_threads - 1) / n_threads;
+                thread_begin = thread_id * items_per_thread;
+                thread_end = std::min((thread_id + 1) * items_per_thread,
+                                      current_n_items);
+              }
+
+              DEBUG_LOG(std::this_thread::get_id() << ": starting task")
+              m_task(thread_begin, thread_end);
+              DEBUG_LOG(std::this_thread::get_id() << ": finished task")
             }
-          }
-          if (m_stop) {
-            break;
-          }
-          m_tasks_ready[thread_id] = false;
-
-          if (auto current_n_items = m_current_n_items.load();
-              current_n_items != old_n_items) {
-            old_n_items = current_n_items;
-            auto items_per_thread =
-                (current_n_items + n_threads - 1) / n_threads;
-            thread_begin = thread_id * items_per_thread;
-            thread_end =
-                std::min((thread_id + 1) * items_per_thread, current_n_items);
-          }
-
-          DEBUG_LOG(std::this_thread::get_id() << ": starting task")
-          m_task(thread_begin, thread_end);
-          DEBUG_LOG(std::this_thread::get_id() << ": finished task")
-        }
-      });
+          },
+          stop_tkn);
     }
   }
 
   ~ThreadPool() {
-    m_stop = true;
-
-    for (auto& t : m_threads) {
-      t.join();
-    }
+    m_stop_source.request_stop();
+    // m_threads.front().request_stop();
+    // for (auto& t : m_threads) {
+    //  t.request_stop();
+    //}
+    DEBUG_LOG(std::this_thread::get_id()
+              << ": in ~ThreadPool; stop requested: "
+              << m_stop_source.stop_requested()
+              << "; stop possible: " << m_stop_source.stop_possible())
+    // std::exit(0);
   }
 
   template <typename ParallelKernel, typename... Args>
@@ -84,9 +100,9 @@ class ThreadPool {
   }
 
  private:
+  std::stop_source m_stop_source{};
   std::vector<std::jthread> m_threads{};
   std::function<void(std::size_t, std::size_t)> m_task{};
   std::vector<std::atomic_bool> m_tasks_ready{};
   std::atomic_int m_current_n_items{};
-  std::atomic_bool m_stop{};
 };
